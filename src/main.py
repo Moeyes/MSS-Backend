848df6881f3e54f0a64c2cd918bede077b80a490
RatanakANB <rattanakphok9@gmail.com>
2025-12-26 16:48:55 +0700

    modify app setting on src/main.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from pathlib import Path
 
 app = FastAPI()
 
 @app.get("/")
 async def read_root():
-    return {"Hello": "World"}
+    return {"Hello": "Worldh"}
+
+@app.get("/favicon.ico", include_in_schema=False)
+async def get_favicon():
+    return FileResponse(Path(__file__).parent.parent / "favicon.ico")
